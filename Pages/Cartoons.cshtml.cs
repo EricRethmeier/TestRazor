751ac028113ccccc8ecf06db1cd14bb67c6f6bd5
EricRethmeier <eric@kredit.nl>
2026-04-22 12:46:02 +0200

    add cartoons (#2)

---
+++ Pages/Cartoons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestRazor.Pages
+{
+    public class CartoonsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
