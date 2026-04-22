db2d34b1b518459d5a1acb011f88da9cb0d67f03
Eric Rethmeier <eric@kredit.nl>
2026-04-22 13:38:45 +0200

    Revert "Dieren (#4)"
    This reverts commit c39b1a9850b5f5bec1becc42d3580c61bd85b45d.

--- Pages/Animals.cshtml.cs
+++ Pages/Animals.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TestRazor.Pages
 {
-    public class EgypteModel : PageModel
+    public class Index1Model : PageModel
     {
         public void OnGet()
         {
