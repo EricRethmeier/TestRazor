c39b1a9850b5f5bec1becc42d3580c61bd85b45d
EricRethmeier <eric@kredit.nl>
2026-04-22 13:00:02 +0200

    Dieren (#4)
    * oude zooi verwijderen
    
    * egypte

--- Pages/Egypte.cshtml.cs
+++ Pages/Egypte.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TestRazor.Pages
 {
-    public class Index1Model : PageModel
+    public class EgypteModel : PageModel
     {
         public void OnGet()
         {
